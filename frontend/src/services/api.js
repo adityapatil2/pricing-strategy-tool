b62ff51bd20ccf844b122fdf329e2d0d6248a356
adityapatil2 <patiladitya416@gmail.com>
2026-04-19 23:55:54 +0530

    update API URL to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:5000'
+const BASE_URL = 'https://pricing-strategy-tool-api.onrender.com'
 const UPLOAD_TOKEN_KEY = 'uploadToken'
 
 const api = axios.create({
